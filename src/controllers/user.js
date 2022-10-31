77e6074ead91e792c858d96066591ae90e4f58e4
pedroKahane <pedrok@datosclaros.com>
2022-10-31 20:00:12 -0300

    pagina terminada

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -63,6 +63,9 @@ module.exports = {
                     });
                 } else{
                     try {
+                        const code = uuidv4();
+                        const email = req.body.email;
+                        const token = getToken({ email, code });
                         const result =req.file != undefined ?  await cloudinary.v2.uploader.upload(req.file.path) : "https://res.cloudinary.com/hmc4uxpzk/image/upload/v1667155509/default_irgdp8.jpg";
                         db.User.create( {
                             email : req.body.email,
@@ -87,9 +90,6 @@ module.exports = {
                                 user_id: usuario.user_id,
                             })
                         });
-                        const code = uuidv4();
-                        const email = req.body.email;
-                        const token = getToken({ email, code });
                         const template = getTemplate(req.body.userName, token);
                         await sendEmail(email, 'Confirma tu cuenta de Prode PLV', template);
                     } catch (error) {
