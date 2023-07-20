c10f13b938dbd37b696592e78606883f6e0dd05c
PedroKahane <pedrok@datosclaros.com>
2023-07-20 14:19:27 -0300

    agregue config

--- src/data/config/config.js
+++ src/data/config/config.js
@@ -1,23 +1,26 @@
 module.exports = {
   "development": {
-    "username": "bdba375dfb6680",
-    "password": "3da9b13b",
-    "database": "heroku_c75d50632c4143c",
-    "host": "us-cdbr-east-06.cleardb.net",
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port": process.env.PORT,
     "dialect": "mysql",
   },
   "test": {
-    "username": "bdba375dfb6680",
-    "password": "3da9b13b",
-    "database": "heroku_c75d50632c4143c",
-    "host": "us-cdbr-east-06.cleardb.net",
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port": process.env.PORT,
     "dialect": "mysql",
   },
   "production": {
-    "username": "bdba375dfb6680",
-    "password": "3da9b13b",
-    "database": "heroku_c75d50632c4143c",
-    "host": "us-cdbr-east-06.cleardb.net",
+    "username": process.env.USERNAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "port": process.env.PORT,
     "dialect": "mysql",
   }
 }
